8844366986e46e1813aed54001d82facd8bf2830
jjackson15 <jacksonjennai81503@gmail.com>
2024-09-23 13:26:47 -0400

    updated values

--- drive_square.py
+++ drive_square.py
@@ -3,6 +3,23 @@ from mbot_bridge.api import MBot
 
 robot = MBot()
 
+"""
+TODO: (P1.1) Write code to make the robot drive in a square. Then,
+modify your code so that the robot drives in a square 3 times.
+
+HINT: A function to send velocity commands to the robot is provided. To
+use it, use the following code:
+
+    robot.drive(vx, vy, wz);
+
+Replace vx, vy, and wz with the velocity in the x direction (vx), y
+direction (vy), and the angular velocity (wz). You can also use this code:
+
+    time.sleep(secs);
+
+to sleep for "secs" seconds (replace with desired number of seconds).
+"""
+
 def drive_square():
     for _ in range(4):  # Four sides of a square
         robot.drive(1.0, 0, 0)  # Move forward (vx=1.0), no sideways (vy=0), no rotation (wz=0)
